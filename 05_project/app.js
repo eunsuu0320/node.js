db42c811c349e3ce6133b9f5c8034c1fbcb36b50
Choi Eunsu <zxzx320@naver.com>
2025-07-08 14:12:51 +0900

    0708

--- 05_project/app.js
+++ 05_project/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 require("dotenv").config({ path: "./mysql/.env" });
 const fs = require("fs");
+const path = require("path");
 
 const { query } = require("./mysql/index.js");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+// 업로드 경로확인
+let uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 // body-parser
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.listen(3000, () => {
   console.log("npm install");
@@ -17,6 +24,10 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
   res.send("Root Router");
+});
+
+app.get("/fileupload", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // 다운로드
@@ -37,6 +48,31 @@ app.get("/download/:productId/:fileName", (req, res) => {
 });
 
 // 업로드
+app.post("/upload/:filename/:pid", (req, res) => {
+  const { filename, pid } = req.params;
+  //  const filePath = `${__dirname}/uploads/${filename}`;
+  let productDir = path.join(uploadDir, pid);
+  if (!fs.existsSync(productDir)) {
+    fs.mkdirSync(productDir);
+  }
+
+  const safeFilename = path.basename(filename); // 경로공격
+  const filePath = path.join(uploadDir, pid, safeFilename);
+
+  try {
+    let base64Data = req.body.data;
+    let data = req.body.data.slice(req.body.data.indexOf(";base64,") + 8); // base64 이후의 글자를 잘라오기위해 + 8을 씀.
+    fs.writeFile(filePath, data, "base64", (err) => {
+      if (err) {
+        res.send("error");
+      } else {
+        res.send("success");
+      }
+    });
+  } catch (err) {
+    res.send("error");
+  }
+});
 
 // 데이터 쿼리
 app.post("/api/:alias", async (req, res) => {
